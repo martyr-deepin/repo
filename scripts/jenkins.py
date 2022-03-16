a6a293e75438371547f6819848f72eea1987724b
justforlxz <justforlxz@gmail.com>
2022-03-16 10:55:44 +0800

    update

--- scripts/jenkins.py
+++ scripts/jenkins.py
@@ -5,44 +5,60 @@ import sys
 import getopt
 import subprocess
 
-
 class Args:
-    def __init__(self, dir, sha):
-        self.dir = dir
+    def __init__(self, name, sha, dir):
+        self.name = name
         self.sha = sha
+        self.dir = dir
+        self.cwd = ''
+        for file in os.listdir(dir):
+            with open(os.path.join(dir, "{}/{}".format(file, 'PKGBUILD')), 'r') as f:
+                lines = f.readlines()
+                for line in lines:
+                    line = line.strip()
+                    if not line.startswith('url') and not line.startswith('URL'):
+                        continue
+                    if 'github.com/linuxdeepin/{}'.format(name) in line:
+                        self.cwd = os.path.join(dir, file)
+                        break
+            if len(self.cwd) != 0:
+                break
+
 def updatePKGBUILD(args: Args):
     lines = []
-    with open("{}/PKGBUILD".format(args.dir), 'r') as f:
+    with open("{}/PKGBUILD".format(args.cwd), 'r') as f:
         for line in f:
             lines.append(line)
     lines = lines[:5] + ['sha={}\n'.format(args.sha)] + lines[5:]
-    with open("{}/PKGBUILD".format(args.dir), 'w') as f:
+    with open("{}/PKGBUILD".format(args.cwd), 'w') as f:
         for line in lines:
             f.write(line)
 
 def build(args: Args):
     commands = [['deepincn-x86_64-build']]
     for command in commands:
-        subprocess.run(command, cwd=args.dir)
+        subprocess.run(command, cwd=args.cwd)
 
 def main(argv):
-    dir = ''
+    name = ''
     sha = ''
+    dir = ''
     try:
-        opts, args = getopt.getopt(argv, "hd:s:", ["dir=", "sha="])
+        opts, args = getopt.getopt(argv, "hn:s:d:", ["name=", "sha=", "dir="])
     except getopt.GetoptError:
-        print('jenkins.py -d <PKGBUILD dir> -s <sha of commit>')
+        print('jenkins.py -n <repo name> -s <sha of commit> -d <repo dir>')
         sys.exit(2)
     for opt, arg in opts:
         if opt == '-h':
-            print('jenkins.py -d <PKGBUILD dir> -s <sha of commit>')
+            print('jenkins.py -n <repo name> -s <sha of commit> -d <repo dir>')
             sys.exit()
-        elif opt in ("-d", "--dir"):
-            dir = os.path.abspath(arg)
+        elif opt in ("-n", "--name"):
+            name = arg
         elif opt in ("-s", "--sha"):
             sha = arg
-    return Args(dir, sha)
-
+        elif opt in ("-d", "--dir"):
+            dir = os.path.abspath(arg)
+    return Args(name, sha, dir)
 
 if __name__ == "__main__":
     args = main(sys.argv[1:])
